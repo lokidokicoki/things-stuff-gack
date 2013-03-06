6e4e381897cd5c2975e6e0182eb115b12992bc15
Robin Tregaskis <me@lokidokicoki.com>
2013-03-06 20:32:37 +0000

    js/utils: new `makeGrid` function, constructs empty 2D array for specified width/height. new `plusOrMinus`, generates a random sign

--- js/utils.js
+++ js/utils.js
@@ -4,8 +4,25 @@ LDC.Utils = (function () {
 	var self = null;
 
 	return {
+		makeGrid : function (x,y) {
+			// make grid
+			var grid = [];
+			for (var i = 0; i < x; i++) {
+				grid[i] = [];
+				for (var j = 0; j < y; j++) {
+					grid[i][j] = null;//0;//getRandomInt(0,1);
+				}
+			}
+			return grid;
+		},
+
 		getRandomInt : function (min, max) {
 		  return Math.floor(Math.random() * (max - min + 1)) + min;
+		},
+		plusOrMinus : function() {
+			var r =  Math.random() < 0.5 ? -1 : 1;
+//			console.debug(r);
+			return r;
 		}
 	};
 });
