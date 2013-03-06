3269af9d68e6bd870fc8bfc222f45421d7066593
Robin Tregaskis <me@lokidokicoki.com>
2013-03-06 20:34:58 +0000

    js/stuff: generates initial dish, has `incubate` function to determine if stuffs should spwn. `spawn` picks a random cell and tries to divide into it. couple of helper funcitons. read to do things nearlys

--- js/stuff.js
+++ js/stuff.js
@@ -1,23 +1,135 @@
 var LDC = LDC || {};
 
-LDC.Stuff = (function () {
-	var self = null;
+LDC.Stuff = function () {
+	this.age = 0;
+//	this.position = [0,0];
+};
 
-	function populate (grid) {
+LDC.StuffManager = (function () {
+	var self = null;
+	var grid = null; // where all the 'stuff' instances live
+
+	function check(x,y){
+		var count = 0;
+		if (tsg.stuffMgr.hasStuff(x-1,y-1)) {count++}
+		if (tsg.stuffMgr.hasStuff(x,y-1)) {count++}
+		if (tsg.stuffMgr.hasStuff(x+1,y-1)) {count++}
+		if (tsg.stuffMgr.hasStuff(x-1,y)) {count++}
+		if (tsg.stuffMgr.hasStuff(x+1,y)) {count++}
+		if (tsg.stuffMgr.hasStuff(x-1,y+1)) {count++}
+		if (tsg.stuffMgr.hasStuff(x,y+1)) {count++}
+		if (tsg.stuffMgr.hasStuff(x+1,y+1)) {count++}
+		if (count > 6){
+			return true;
+		}else{
+			return false;
+		}
+
+	};
+	function spawn (x,y,dish, attempt) {
+		var nx = x + (tsg.utils.getRandomInt(0,1) *  tsg.utils.plusOrMinus());
+		var ny = y + (tsg.utils.getRandomInt(0,1) *  tsg.utils.plusOrMinus());
+		//console.debug(x, y, nx,ny);
+		if (nx < 0 || ny < 0 || nx >= tsg.xlen || ny >= tsg.ylen){
+			return false;
+		}
+		if (tsg.stuffMgr.hasStuff(nx,ny)){
+			return false;
+		}
+		
+		dish[nx][ny] = new LDC.Stuff();
+		/*
+		if (tsg.stuffMgr.hasStuff(x-1,y-1)) {count++}else{tsg.stuffMgr.add(dish, x-1,y-1);}
+		if (tsg.stuffMgr.hasStuff(x,y-1)) {count++}else{tsg.stuffMgr.add(dish, x,y-1);}
+		if (tsg.stuffMgr.hasStuff(x+1,y-1)) {count++}else{tsg.stuffMgr.add(dish, x+1,y-1);}
+		if (tsg.stuffMgr.hasStuff(x-1,y)) {count++}else{tsg.stuffMgr.add(dish, x-1,y);}
+		if (tsg.stuffMgr.hasStuff(x+1,y)) {count++}else{tsg.stuffMgr.add(dish, x+1,y);}
+		if (tsg.stuffMgr.hasStuff(x-1,y+1)) {count++}else{tsg.stuffMgr.add(dish, x-1,y+1);}
+		if (tsg.stuffMgr.hasStuff(x,y+1)) {count++}else{tsg.stuffMgr.add(dish, x,y+1);}
+		if (tsg.stuffMgr.hasStuff(x+1,y+1)) {count++}else{tsg.stuffMgr.add(dish, x+1,y+1);}
+		*/
+
+	};
+
+	// initial populate of grid
+	function populate () {
+		grid = tsg.utils.makeGrid(tsg.xlen, tsg.ylen);
+
 		for (var y = 0; y < tsg.ylen; y++){
 			for (var x = 0; x < tsg.xlen; x++){
-				var val = tsg.utils.getRandomInt(0,200);
+				var val = tsg.utils.getRandomInt(0,1000);
 				if (val === 5){
-					grid[x][y] = 1;
+					grid[x][y] = new LDC.Stuff();
 				}
 			}
 		}
+					//grid[25][25] = new LDC.Stuff();
 	};
 
 	return {
-		init : function (grid) {
+		init : function () {
 			self = this;
-			populate(grid);
+			populate();
+		},
+		hasStuff : function (x,y) {
+			if (x < 0 || y < 0 || x >= tsg.xlen || y >= tsg.ylen){
+				return false;
+			}
+			var s = null;
+			try{
+				s = grid[x][y];
+			}catch (e){
+				console.log(e);
+				console.debug(x,y);
+			}
+			return s;
+		},
+		add : function (dish, x,y) {
+			if (x < 0 || y < 0 || x >= tsg.xlen || y >= tsg.ylen){
+				return false;
+			}
+			dish[x][y] = new LDC.Stuff();
+		},
+
+		incubate : function () {
+			var next = tsg.utils.makeGrid(tsg.xlen, tsg.ylen);
+
+			for (var x = 0; x < tsg.xlen; x++){
+				for (var y = 0; y < tsg.ylen; y++){
+					var stuff = grid[x][y];
+					if (stuff && stuff !== undefined){
+						next[x][y] = stuff;
+						if (stuff.age%3 == 0) {
+							spawn(x,y, next);
+						}
+						if (check(x,y)){
+							stuff.age=999;
+						}
+						stuff.age++;
+						
+						if (stuff.age >= 10) {
+							delete stuff;
+							next[x][y]=null;
+						}
+					}
+				}
+			}
+
+			grid = next;
+		},
+		draw : function (ctx, x,y) {
+			var stuff = grid[x][y];
+			if (stuff && stuff !== undefined){
+				var age = stuff.age;
+				if (age <= 3) {
+					ctx.fillStyle = "rgb(0,255,0)";
+				}else if (age <=6) { 
+					ctx.fillStyle = "rgb(0,128,0)";
+				}else{
+					ctx.fillStyle = "rgb(0,64,0)";
+				}
+			}
+			return ctx;
 		}
 	};
 });
