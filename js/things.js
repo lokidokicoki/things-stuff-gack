516e2a0ff1c89d80a875a9ff53b0b505592f4c98
Robin Tregaskis <me@lokidokicoki.com>
2013-03-24 12:20:07 +0000

    js/things: new storage, draw mechanisms...
    - store thing instances in an object instead of a grid
    - loop over this object in draw (much quicker)
    - new `nearest` function, find Thing in store nearest passed point
    - new hasThing, might be redundant...
    - Thing can be marked as `selected` changes coloiur and call TSG.info
    
    TODO:
    - save/load thing store
    - genes, movement rules etc.

--- js/things.js
+++ js/things.js
@@ -1,27 +1,27 @@
 var LDC = LDC || {};
 
-LDC.Thing = function (id,x,y) {
-	this.id = id;
+LDC.Thing = function (uid,x,y) {
+	this.uid = uid;
 	this.age = 0;
 	this.energy = 300;
 	this.speed = 1;
 	this.eaten = false;
 	this.position = [x,y];
+	this.selected = false;
 };
 
 LDC.ThingManager = (function () {
 	var self = null;
 	var scale = 1;
-	var grid = null; // where all the 'thing' instances live
 	var rad360 = Math.PI * 2;
 	var counter = 0;
+	var store = {}; //where all thing instance live
 
-	function move(thing, x,y, dish) {
-		var nx = x + (tsg.utils.getRandomInt(0,thing.speed) *  tsg.utils.plusOrMinus());
-		var ny = y + (tsg.utils.getRandomInt(0,thing.speed) *  tsg.utils.plusOrMinus());
+	function move(thing) {
+		var x = thing.position[0] + (tsg.utils.getRandomInt(0,thing.speed) *  tsg.utils.plusOrMinus());
+		var y = thing.position[1] + (tsg.utils.getRandomInt(0,thing.speed) *  tsg.utils.plusOrMinus());
 		
-		dish[nx][ny] = thing;
-		thing.position =[nx,ny]
+		thing.position =[x,y]
 		thing.energy -= thing.speed;
 	};
 
@@ -33,22 +33,22 @@ LDC.ThingManager = (function () {
 			return false;
 		}
 		
-		dish[nx][ny] = new LDC.Thing(counter++, nx, ny);
+		var thing = new LDC.Thing(counter++, nx, ny);
+		store[thing.uid] = thing;
 	};
 
 	// initial populate of grid
 	function populate () {
-		grid = tsg.utils.makeGrid(tsg.xlen, tsg.ylen);
-
 		for (var y = 0; y < tsg.ylen; y++){
 			for (var x = 0; x < tsg.xlen; x++){
 				var val = tsg.utils.getRandomInt(0,2000);
 				if (val === 5){
-					grid[x][y] = new LDC.Thing(counter++, x,y);
+					var thing = new LDC.Thing(counter++, x,y);
+					store[thing.uid] = thing;
+
 				}
 			}
 		}
-		//grid[25][25] = new LDC.Thing();
 	};
 
 	return {
@@ -59,68 +59,94 @@ LDC.ThingManager = (function () {
 		},
 
 		incubate : function () {
-			var next = tsg.utils.makeGrid(tsg.xlen, tsg.ylen);
+			for (var uid in store){
+				var thing = store[uid];
+				if (thing && thing !== undefined){
+					// spawn
+					/*
+					if (thing.age%15 == 0) {
+						spawn(x,y, next);
+					}
+					*/
 
-			for (var x = 0; x < tsg.xlen; x++){
-				for (var y = 0; y < tsg.ylen; y++){
-					var thing = grid[x][y];
-					if (thing && thing !== undefined){
-						// born
-						if (thing.age%15 == 0) {
-							//spawn(x,y, next);
-							//console.debug('thing.spawn');
-						}
+					// eat
+					thing.eaten = false;
+					if (tsg.stuffMgr.hasStuff(thing.position)){
+						thing.energy+=3;
+						thing.eaten = true;
+						tsg.stuffMgr.killStuff(thing.position);
+					}else{
+						move(thing);
+					}
 
-						thing.eaten = false;
-						// eat
-						if (tsg.stuffMgr.hasStuff(x,y)){
-							next[x][y] = thing;
-							thing.energy+=3;
-							thing.eaten = true;
-							tsg.stuffMgr.killStuff(x,y);
-						}else{
-							move(thing, x,y, next);
-						}
-
-						// shag
-						thing.age++;
-						
-						// die
-						if (thing.energy < 0) {
-							delete thing;
-							next[x][y]=null;
-						}
+					// shag
+					thing.age++;
+					
+					// die
+					if (thing.energy < 0) {
+						store[thing.uid] = null;
+						delete thing;
 					}
 				}
 			}
-
-			grid = next;
 		},
 
-		hasThing : function (x,y) {
-			var thing = grid[x][y];
-			if (thing && thing !== undefined){
-				return thing;
-			}else{
-				return null;
+		hasThing : function (point) {
+			var thing = null;
+			for (var uid in store){
+				var t = store[uid];
+				if (t.position[0] == point[0] && t.position[1] == point[1])	{
+					thing = t;
+					break;
+				}
 			}
+			return thing;
+		},
+
+		sqr : function (v) {
+			return v*v;
+		},
+
+		nearest : function (point) {
+			var minima = 99999999;
+			var result = null;
+			for (var uid in store){
+				var thing = store[uid];
+				thing.selected = false;
+				var x2 = Math.abs(point[0] - thing.position[0]);
+				var y2 = Math.abs(point[1] - thing.position[1]);
+				var dist = Math.sqrt(self.sqr(x2) + self.sqr(y2));
+				if (dist < minima) {
+					minima = dist;
+					result = [thing, dist];	
+				}
+			}
+
+			return result;
 		},
 
 		draw : function (ctx, x,y) {
-			var thing = grid[x][y];
-			if (thing && thing !== undefined){
-				ctx.beginPath();
-				var fill='';
-				var energy = thing.energy;
-				if (energy > 200) {
-					ctx.fillStyle = "rgb(255,0,0)";
-				}else if (energy > 100) { 
-					ctx.fillStyle = "rgb(128,0,0)";
-				}else{
-					ctx.fillStyle = "rgb(64,0,0)";
+			for (var uid in store){
+				var thing = store[uid];
+				if (thing && thing !== undefined){
+					var select = 0;
+					if (thing.selected){
+						select = 255;
+						tsg.info(thing);
+					}
+					ctx.beginPath();
+					var fill='';
+					var energy = thing.energy;
+					if (energy > 200) {
+						ctx.fillStyle = "rgb(255,"+select+",0)";
+					}else if (energy > 100) { 
+						ctx.fillStyle = "rgb(128,"+select+",0)";
+					}else{
+						ctx.fillStyle = "rgb(64,"+select+",0)";
+					}
+					ctx.arc(thing.position[0]*scale,thing.position[1]*scale, 1*scale, 0, rad360);
+					ctx.fill();
 				}
-				ctx.arc(x*scale,y*scale, 1*scale, 0, rad360);
-				ctx.fill();
 			}
 			return ctx;
 		}
